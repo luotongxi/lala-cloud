16b63cae21ec7369cff676897316ae42febb5366
luotong <18515050249@163.com>
2017-06-23 14:03:34 +0800

    add user microservice

---
+++ microservice/user/src/main/java/com/tluo/lala/microservice/users/Application.java
@@ -0,0 +1,20 @@
+package com.tluo.lala.microservice.users;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by luotong on 2017/6/23.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+    private static Logger logger = Logger.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        logger.info("============= LaLa Users Service Start Success =============");
+    }
+}
