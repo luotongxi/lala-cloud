3446faea130113775458ed6f6a8ce7f06194cf07
luotong <18515050249@163.com>
2017-07-31 08:45:27 +0800

    消费服务修改

--- microservice/user/src/main/java/com/tluo/lala/microservice/users/controller/UserController.java
+++ microservice/user/src/main/java/com/tluo/lala/microservice/users/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.tluo.lala.microservice.users.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,9 @@ public class UserController {
     @Value("${user.name}")
     private String uname;
 
-    @RequestMapping(value = "/name",method = RequestMethod.GET)
-    public String get(){
+    @RequestMapping(value = "/name", method = RequestMethod.GET)
+    @ApiOperation(value = "用户姓名", notes = "查询用户的姓名")
+    public String get() {
         return this.uname;
     }
 }
