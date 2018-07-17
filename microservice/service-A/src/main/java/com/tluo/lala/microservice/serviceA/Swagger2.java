77c5db14e827a31c22bcf715985c6679968d67fb
luotong <luotong@100tal.com>
2018-07-17 14:34:31 +0800

    添加serviceA服务

---
+++ microservice/service-A/src/main/java/com/tluo/lala/microservice/serviceA/Swagger2.java
@@ -0,0 +1,39 @@
+package com.tluo.lala.microservice.serviceA;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by edz on 2017/7/30.
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("spring cloud 相关学习博客http://blog.didispace.com/categories/Spring-Cloud/")
+//                .termsOfServiceUrl("http://blog.didispace.com/")
+                .contact("铜锣金")
+                .version("1.0")
+                .build();
+    }
+}
